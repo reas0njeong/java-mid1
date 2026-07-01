92e5aff1286ed265e21424ed9db1ad6a69731792
reas0njeong <seahorse2001@naver.com>
2026-07-01 14:17:01 +0900

    sec04: 메서드 체인닝 - Method Chaining

---
+++ src/lang/string/chianing/MethodChainingMain1.java
@@ -0,0 +1,16 @@
+package lang.string.chianing;
+
+public class MethodChainingMain1 {
+
+    public static void main(String[] args) {
+        ValueAdder adder = new ValueAdder();
+        adder.add(1);
+        adder.add(2);
+        adder.add(3);
+        adder.add(4);
+
+        int result = adder.getValue();
+        System.out.println("result = " + result);
+
+    }
+}
