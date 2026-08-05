bb97b0367c7f5c03d3dd648d23d51a4eb1d3ec57
reas0njeong <seahorse2001@naver.com>
2026-08-05 12:36:01 +0900

    sec09: 지역 클래스 - 지역 변수 캡처2

--- src/nested/local/LocalOuterV3.java
+++ src/nested/local/LocalOuterV3.java
@@ -1,5 +1,8 @@
 package nested.local;
 
+import java.lang.reflect.Field;
+import java.sql.SQLOutput;
+
 public class LocalOuterV3 {
 
     private int outInstanceVar = 3;
@@ -32,5 +35,12 @@ public class LocalOuterV3 {
         Printer printer = localOuter.process(2);
         // printer.print()를 나중에 실행한다. process()의 스택 프레임이 사라진 이후에 실행
         printer.print();
+
+        // 추가
+        System.out.println("필드 확인");
+        Field[] declaredFields = printer.getClass().getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            System.out.println("declaredField = " + declaredField);
+        }
     }
 }
