1154b72c076c7d6fc340db20819703ef3688b20a
reas0njeong <seahorse2001@naver.com>
2026-08-05 13:13:05 +0900

    sec09: 지역 클래스 - 지역 변수 캡처3

--- src/nested/local/LocalOuterV4.java
+++ src/nested/local/LocalOuterV4.java
@@ -2,7 +2,7 @@ package nested.local;
 
 import java.lang.reflect.Field;
 
-public class LocalOuterV3 {
+public class LocalOuterV4 {
 
     private int outInstanceVar = 3;
 
@@ -24,13 +24,14 @@ public class LocalOuterV3 {
         }
 
         LocalPrinter printer = new LocalPrinter();
-        // printer.print();를 여기서 실행하지 않고 Printer 인스턴스만 반환한다.
+        // 만약 localVar의 값을 변경한다면?
+        // localVar = 10;
 
         return printer;
     }
 
     public static void main(String[] args) {
-        LocalOuterV3 localOuter = new LocalOuterV3();
+        LocalOuterV4 localOuter = new LocalOuterV4();
         Printer printer = localOuter.process(2);
         // printer.print()를 나중에 실행한다. process()의 스택 프레임이 사라진 이후에 실행
         printer.print();
